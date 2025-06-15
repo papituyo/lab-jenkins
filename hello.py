2cb05a6d9016042501786819e664099f98355b2e
jesusedt1 <jesus.delgado@alumnos.ipleones.cl>
2025-06-14 20:44:20 -0400

    Update hello.py
    Cambio 1 , se agrega linea

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 def saludar():
     print("Hola, esto es una prueba de integración continua.")
+    print("Hola, este es un cambio para probar Jenkins.")
+
 
 if __name__ == "__main__":
     saludar()
