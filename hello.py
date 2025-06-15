f401b290962d405239ba818035a28f12355735fe
jesusedt1 <jesus.delgado@alumnos.ipleones.cl>
2025-06-14 21:36:01 -0400

    Update hello.py
    se agrega linea para validar cambio

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ def saludar():
     print("Hola, esto es una prueba de integración continua.")
     print("Hola, este es un cambio para probar Jenkins.")
     print("Hola, este es un cambio 2 para probar Jenkins.")
+    print("Hola, este es un cambio 3 para probar Jenkins.")
 
 
 if __name__ == "__main__":
